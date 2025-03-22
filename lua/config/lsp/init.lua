abeb7523347fc0885eaabda868547f19bb47dd24
ChuseCubr <chasevillarroel@gmail.com>
2025-03-22 10:54:13 +0800

    restructure lsp setup

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,3 +1,6 @@
+local lsp = require("utils.lsp")
+lsp.setup()
+
 local lsp_configs_path = vim.fn.stdpath("config") .. "/lua/config/lsp"
 
 for name, type in vim.fs.dir(lsp_configs_path) do
@@ -6,44 +9,3 @@ for name, type in vim.fs.dir(lsp_configs_path) do
 		require("config.lsp." .. name)
 	end
 end
-
-local group = vim.api.nvim_create_augroup("GenericLspAttach", { clear = true })
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = group,
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client == nil then return end
-
-		-- navigation
-		vim.keymap.set("n", "gd", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-		vim.keymap.set("i", "gD", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
-
-		-- TODO: TO BE DEPRECATED, will become defaults in 0.11
-		-- hover
-		vim.keymap.set({"s", "i"}, "<c-s>", vim.lsp.buf.signature_help, { desc = "Signature help" })
-
-		-- navigation
-		vim.keymap.set("n", "grr", vim.lsp.buf.references, { desc = "Go to references" })
-		vim.keymap.set("n", "gri", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-
-		-- list
-		vim.keymap.set("n", "gO",
-			function() vim.lsp.buf.document_symbol({ loclist = true }) end,
-			{ desc = "Document symbols" }
-		)
-
-		-- actions
-		vim.keymap.set("n", "grn", vim.lsp.buf.rename, { desc = "Rename symbol" })
-		vim.keymap.set("n", "gra", vim.lsp.buf.code_action, { desc = "Code action" })
-
-		vim.schedule(function()
-			vim.api.nvim_echo({
-				{
-					"LSP Client for `" .. client.name .. "` successfully attached",
-					"DiagnosticOk",
-				}
-			}, true, {})
-		end)
-	end,
-})
--- lua/utils/lsp.lua
+++ lua/utils/lsp.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+
+-- SETTINGS
+
+---LSP autocmd group
+local group = vim.api.nvim_create_augroup("UnpluggedLsp", { clear = true })
+
+---Buffer types to not init LSP on
+---@type string[]
 local do_not_attach_types = {
 	"help",
 	"nofile",
@@ -8,9 +16,22 @@ local do_not_attach_types = {
 	"prompt",
 }
 
-function M.find_root(markers)
+
+-- PRIVATE FIELDS
+
+---Setup guard
+local setup = false
+
+
+-- PRIVATE METHODS / UTILS
+
+---@param markers string[] List of root marker files
+local function find_root(markers)
 	return vim.fs.dirname(vim.fs.find(markers, { upward = true })[1])
 end
+
+
+-- PUBLIC METHODS
 
 ---@param config (vim.lsp.ClientConfig | { root_markers: string[] })
 function M.init_lsp_client(config, opts)
@@ -22,24 +43,70 @@ function M.init_lsp_client(config, opts)
 		opts = opts or {}
 
 		config.name = config.cmd[1]
-		config.root_dir = M.find_root(config.root_markers)
+		config.root_dir = find_root(config.root_markers)
 		opts.silent = true
 
 		local client = vim.lsp.start(config, opts)
 		if client == nil then
 			vim.schedule(function()
-				vim.api.nvim_echo({
-					{
-						"Failed to start LSP Server for " .. config.name,
-						"DiagnosticError",
-					}
-				}, false, {})
+				vim.notify(
+					"Failed to start LSP Server for `" .. config.name .. "`",
+					vim.log.levels.ERROR
+				)
 			end)
 			return
 		end
 
 		vim.lsp.buf_attach_client(0, client)
 	end
+end
+
+---Sets up LspAttach autocmd
+function M.setup()
+	if setup then return end
+	setup = true
+
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = group,
+		callback = function(args)
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			if client == nil then return end
+
+			-- navigation
+			vim.keymap.set("n", "gd", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+			vim.keymap.set("i", "gD", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+
+			-- TODO: TO BE DEPRECATED, will become defaults in 0.11
+			-- hover
+			vim.keymap.set({ "s", "i" }, "<c-s>", vim.lsp.buf.signature_help, { desc = "Signature help" })
+
+			-- navigation
+			vim.keymap.set("n", "grr", vim.lsp.buf.references, { desc = "Go to references" })
+			vim.keymap.set("n", "gri", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+
+			-- list
+			vim.keymap.set("n", "gO",
+				function() vim.lsp.buf.document_symbol({ loclist = true }) end,
+				{ desc = "Document symbols" }
+			)
+
+			-- actions
+			vim.keymap.set("n", "grn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+			vim.keymap.set("n", "gra", vim.lsp.buf.code_action, { desc = "Code action" })
+
+			vim.schedule(function()
+				vim.notify(
+					string.format(
+						"LSP client for `%s` successfully attached to buffer %d (%s)",
+						client.name,
+						args.buf,
+						vim.fn.fnamemodify(args.file, ":.")
+					),
+					vim.log.levels.INFO
+				)
+			end)
+		end,
+	})
 end
 
 return M
