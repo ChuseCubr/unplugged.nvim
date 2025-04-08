b45eb0758cc338526c48ef36c6f97eb7a50c52f7
ChuseCubr <chasevillarroel@gmail.com>
2025-04-08 19:56:52 +0800

    tweak settings (netrw and folds)

--- lua/config/options/editor.lua
+++ lua/config/options/editor.lua
@@ -26,6 +26,12 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 
+-- -- fold options
+vim.o.foldenable = true
+vim.o.foldmethod = 'indent'
+vim.o.foldlevel = 99
+vim.o.foldcolumn = '1'
+
 -- keymap behavior
 vim.o.timeoutlen = 300
 
@@ -41,7 +47,9 @@ vim.o.splitright = true
 
 -- netrw
 vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 1
+vim.g.netrw_liststyle = 3
+vim.g.netrw_preview = 1
+vim.g.netrw_winsize = 20
 vim.g.netrw_sizestyle = "H"
 vim.g.netrw_sort_sequence = "^\\f\\+[\\/]"
 
