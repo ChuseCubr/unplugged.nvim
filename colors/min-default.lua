2cb74489ac65a13e0003450c05c821efa934d276
ChuseCubr <chasevillarroel@gmail.com>
2025-05-07 11:01:11 +0800

    Fix colorscheme when starting in light mode

--- colors/min-default.lua
+++ colors/min-default.lua
@@ -1,7 +1,3 @@
-vim.cmd("hi clear")
-vim.cmd("syntax reset")
-vim.g.colors_name = "min-default"
-
 local prefix = vim.o.background == "dark" and "NvimLight" or "NvimDark"
 local inverted_prefix = vim.o.background == "dark" and "NvimDark" or "NvimLight"
 
@@ -47,81 +43,6 @@ local function dimmed_color(groups)
 	end
 end
 
--- resets
-local base = {
-	"Boolean",
-	"Float",
-	"Function",
-	"Identifier",
-	"Label",
-	"Macro",
-	"Number",
-	"Tag",
-	"@attribute.builtin",
-	"@constant.builtin",
-	"@function.builtin",
-	"@tag.builtin",
-	"@variable",
-	"@variable.builtin",
-	"@variable.parameter.builtin",
-	"MiniStarterItem",
-}
-
-base_color(base)
-
--- dim
-local dimmed = {
-	"Delimiter",
-	"Operator",
-	"Special",
-	"Structure",
-	"Type",
-	"ComplMatchIns",
-}
-
-dimmed_color(dimmed)
-
--- highlights
-set_color("Comment", "Yellow")
-set_color("Keyword", "Blue")
-
-set_color("String", "Green")
-set_color("Character", "Green")
-set_color("SpecialChar", "Green")
-
-set_color("DiagnosticError", "Red")
-set_color("DiagnosticWarn", "Yellow")
-set_color("DiagnosticInfo", "Cyan")
-
-set_color("Added", "Green")
-set_color("Removed", "Red")
-set_color("Changed", "Yellow")
-
-set_accent("Todo", "Magenta")
-set_accent("Fixme", "Red")
-
--- manual sets
-vim.api.nvim_set_hl(0, "MatchParen", { fg = prefix .. "Cyan", bg = inverted_prefix .. "Blue" })
-
--- neovide term colors
-vim.g.terminal_color_0 = prefix .. "Gray4"
-vim.g.terminal_color_1 = prefix .. "Red"
-vim.g.terminal_color_2 = prefix .. "Green"
-vim.g.terminal_color_3 = prefix .. "Yellow"
-vim.g.terminal_color_4 = prefix .. "Blue"
-vim.g.terminal_color_5 = prefix .. "Magenta"
-vim.g.terminal_color_6 = prefix .. "Cyan"
-vim.g.terminal_color_7 = prefix .. "Gray2"
-
-vim.g.terminal_color_8 = vim.g.terminal_color_0
-vim.g.terminal_color_9 = vim.g.terminal_color_1
-vim.g.terminal_color_10 = vim.g.terminal_color_2
-vim.g.terminal_color_11 = vim.g.terminal_color_3
-vim.g.terminal_color_12 = vim.g.terminal_color_4
-vim.g.terminal_color_13 = vim.g.terminal_color_5
-vim.g.terminal_color_14 = vim.g.terminal_color_6
-vim.g.terminal_color_15 = vim.g.terminal_color_7
-
 -- statusline and winbar
 local function set_statusline_color(name, color)
 	color = "NvimLight" .. color
@@ -139,14 +60,97 @@ local function set_winbar_color(name, color)
 	vim.api.nvim_set_hl(0, name, { fg = fg, bg = bg })
 end
 
-set_statusline_color("StatusLineGreen", "Green")
-set_statusline_color("StatusLineYellow", "Yellow")
-set_statusline_color("StatusLineRed", "Red")
-set_statusline_color("StatusLineBlue", "Blue")
-set_statusline_color("StatusLineCyan", "Cyan")
+local function setup()
+	vim.cmd("hi clear")
+	vim.cmd("syntax reset")
+	vim.g.colors_name = "min-default"
 
-set_winbar_color("WinBarGreen", "Green")
-set_winbar_color("WinBarYellow", "Yellow")
-set_winbar_color("WinBarRed", "Red")
-set_winbar_color("WinBarBlue", "Blue")
-set_winbar_color("WinBarCyan", "Cyan")
+	-- resets
+	local base = {
+		"Boolean",
+		"Float",
+		"Function",
+		"Identifier",
+		"Label",
+		"Macro",
+		"Number",
+		"Tag",
+		"@attribute.builtin",
+		"@constant.builtin",
+		"@function.builtin",
+		"@tag.builtin",
+		"@variable",
+		"@variable.builtin",
+		"@variable.parameter.builtin",
+		"MiniStarterItem",
+	}
+
+	base_color(base)
+
+	-- dim
+	local dimmed = {
+		"Delimiter",
+		"Operator",
+		"Special",
+		"Structure",
+		"Type",
+		"ComplMatchIns",
+	}
+
+	dimmed_color(dimmed)
+
+	-- highlights
+	set_color("Comment", "Yellow")
+	set_color("Keyword", "Blue")
+
+	set_color("String", "Green")
+	set_color("Character", "Green")
+	set_color("SpecialChar", "Green")
+
+	set_color("DiagnosticError", "Red")
+	set_color("DiagnosticWarn", "Yellow")
+	set_color("DiagnosticInfo", "Cyan")
+
+	set_color("Added", "Green")
+	set_color("Removed", "Red")
+	set_color("Changed", "Yellow")
+
+	set_accent("Todo", "Magenta")
+	set_accent("Fixme", "Red")
+
+	-- manual sets
+	vim.api.nvim_set_hl(0, "MatchParen", { fg = prefix .. "Cyan", bg = inverted_prefix .. "Blue" })
+
+	-- neovide term colors
+	vim.g.terminal_color_0 = prefix .. "Gray4"
+	vim.g.terminal_color_1 = prefix .. "Red"
+	vim.g.terminal_color_2 = prefix .. "Green"
+	vim.g.terminal_color_3 = prefix .. "Yellow"
+	vim.g.terminal_color_4 = prefix .. "Blue"
+	vim.g.terminal_color_5 = prefix .. "Magenta"
+	vim.g.terminal_color_6 = prefix .. "Cyan"
+	vim.g.terminal_color_7 = prefix .. "Gray2"
+
+	vim.g.terminal_color_8 = vim.g.terminal_color_0
+	vim.g.terminal_color_9 = vim.g.terminal_color_1
+	vim.g.terminal_color_10 = vim.g.terminal_color_2
+	vim.g.terminal_color_11 = vim.g.terminal_color_3
+	vim.g.terminal_color_12 = vim.g.terminal_color_4
+	vim.g.terminal_color_13 = vim.g.terminal_color_5
+	vim.g.terminal_color_14 = vim.g.terminal_color_6
+	vim.g.terminal_color_15 = vim.g.terminal_color_7
+
+	set_statusline_color("StatusLineGreen", "Green")
+	set_statusline_color("StatusLineYellow", "Yellow")
+	set_statusline_color("StatusLineRed", "Red")
+	set_statusline_color("StatusLineBlue", "Blue")
+	set_statusline_color("StatusLineCyan", "Cyan")
+
+	set_winbar_color("WinBarGreen", "Green")
+	set_winbar_color("WinBarYellow", "Yellow")
+	set_winbar_color("WinBarRed", "Red")
+	set_winbar_color("WinBarBlue", "Blue")
+	set_winbar_color("WinBarCyan", "Cyan")
+end
+
+vim.schedule(setup)
