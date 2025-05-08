580fc2c81c72306661ea3ce05450f9e6eb786f7a
ChuseCubr <chasevillarroel@gmail.com>
2025-05-08 09:49:45 +0800

    Modify qf/loclist mappings

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -15,5 +15,19 @@ vim.keymap.set("n", "<cr>", function()
 	end)
 end, { noremap = true, buffer = true, desc = "Select entry and close list" })
 
--- select without closing
-vim.keymap.set("n", "p", "<cr>", { buffer = true, noremap = true, desc = "Preview (select without closing)" })
+vim.keymap.set("n", "o", "<cr>", { buffer = true, noremap = true, desc = "Select without closing list" })
+
+vim.keymap.set("n", "p", function()
+	local win = vim.api.nvim_get_current_win()
+
+	-- select entry
+	local keys = vim.api.nvim_replace_termcodes("<cr>", true, false, true)
+	vim.api.nvim_feedkeys(keys, "nt", false)
+
+	-- schedule to ensure it runs after nvim_feedkeys
+	vim.schedule(function()
+		if vim.api.nvim_win_is_valid(win) then
+			vim.api.nvim_set_current_win(win)
+		end
+	end)
+end, { buffer = true, noremap = true, desc = "Preview" })
