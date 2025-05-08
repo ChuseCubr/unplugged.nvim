c4b4e60523c7c7b2fb52e5e40219cddaa152300b
ChuseCubr <chasevillarroel@gmail.com>
2025-05-08 20:40:18 +0800

    Slightly improve Netrw performance

---
+++ lua/utils/netrw.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+-- get SNR of Netrw
+local snr = require("utils.snr")
+snr.setup()
+local netrwSnr = snr.getSnrByPattern("**/autoload/netrw.vim")
+
+local setup = false
+local PMap
+local VMap
+local OMap
+
+-- utils
+local function invertAlts()
+	vim.g.netrw_alto = 1 - vim.g.netrw_alto
+	vim.g.netrw_altv = 1 - vim.g.netrw_altv
+end
+
+local function invertWindowSize()
+	vim.g.netrw_winsize = 100 - vim.g.netrw_winsize
+end
+
+M.PCallback = function()
+	invertAlts()
+	vim.api.nvim_feedkeys(PMap, "nt", false)
+	vim.schedule(invertAlts)
+end
+
+M.VCallback = function()
+	invertWindowSize()
+	vim.api.nvim_feedkeys(VMap, "nt", false)
+	vim.schedule(invertWindowSize)
+end
+
+M.OCallback = function()
+	invertWindowSize()
+	vim.api.nvim_feedkeys(OMap, "nt", false)
+	vim.schedule(invertWindowSize)
+end
+
+M.setup = function()
+	if setup then return end
+
+	-- `nmap` mappings taken from Netrw buffer
+	local PMapTemplate = ":<C-U>call <SNR>%d_NetrwPreview(<SNR>%d_NetrwBrowseChgDir(1,<SNR>%d_NetrwGetWord(),1,1))<CR>"
+	local PMapString = PMapTemplate:gsub("%%d", netrwSnr)
+	PMap = vim.api.nvim_replace_termcodes(PMapString, true, false, true)
+
+	local VMapTemplate = ":call <SNR>%d_NetrwSplit(5)<CR>"
+	local VMapString = VMapTemplate:gsub("%%d", netrwSnr)
+	VMap = vim.api.nvim_replace_termcodes(VMapString, true, false, true)
+
+	local OMapTemplate = ":call <SNR>%d_NetrwSplit(3)<CR>"
+	local OMapString = OMapTemplate:gsub("%%d", netrwSnr)
+	OMap = vim.api.nvim_replace_termcodes(OMapString, true, false, true)
+
+	setup = true
+end
+
+return M
